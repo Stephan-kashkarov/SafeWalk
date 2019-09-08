fe8c1c9e0659dca4068a5e771b1bec2d6a669bec
stephan-kashkarov <kashkarovwork@gmail.com>
2019-09-08 12:24:16 +1000

    added new.csv and added more to busses.py

--- bus.py
+++ bus.py
@@ -2,21 +2,48 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
+print('1')
 stops = pd.concat([
     pd.read_csv(f"data/gtfs (0)/stops.txt"),
     pd.read_csv(f"data/gtfs (1)/stops.txt")
 ])
+print('1')
 times = pd.concat([
-    pd.read_csv(f"data/gtfs (0)/stop_time.txt"),
-    pd.read_csv(f"data/gtfs (1)/stop_time.txt")
+    pd.read_csv(f"data/gtfs (0)/stop_times.txt"),
+    pd.read_csv(f"data/gtfs (1)/stop_times.txt")
 ])
+print('1')
+trip = pd.concat([
+    pd.read_csv(f"data/gtfs (0)/trips.txt"),
+    pd.read_csv(f"data/gtfs (1)/trips.txt")
+])
+print('1')
 out = pd.DataFrame()
 out['id'] = stops.stop_id.unique()
-out['name'] = stops[stops.stop_id == out.id].stop_name
-out['usage'] = times[times.stop_id == out.id].count()
-out['lat'] = stops[stops.stop_id == out.id].stop_lat
-out['long'] = stops[stops.stop_id == out.id].stop_long
+print(type(out.id))
+print(stops[stops.stop_id == 201].stop_name)
+print(2)
+out['name'] = out.apply(lambda row: stops[stops.stop_id == row.id].stop_name.iloc[0], axis=1)
+print(3)
 
+print(4)
+out['lat'] = out.apply(lambda row: stops[stops.stop_id == row.id].stop_lat.iloc[0], axis=1)
+print(5)
+out['lon'] = out.apply(lambda row: stops[stops.stop_id == row.id].stop_lon.iloc[0], axis=1)
+# print(6)
+# out['wheelchair'] = out.apply(lambda row: trip[
+#     trip.stop_id == row.stop_id
+#     and trip.wheelchair_accessible == 1
+# ].count() / trip[trip.stop_id == row.stop_id].count(), axis=1)
 
-with open(f'data/new.csv') as f:
-    pass
+# out['bikes'] = out.apply(lambda row: trip[
+#     trip.stop_id == row.stop_id
+#     and trip.bikes_allowed == 1
+# ].count() / trip[trip.stop_id == row.stop_id].count(), axis=1)
+
+out['fequency'] = out.apply(
+    lambda row: times[times.stop_id == row.id].trip_id.count(), axis=1)
+
+print(out)
+with open(f'data/new.csv', 'w+') as f:
+    f.write(out.to_csv())
